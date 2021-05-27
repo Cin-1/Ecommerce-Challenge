e74b8964fa5433f163e93c63a238cc2ce267255e
Cin-1 <cinthiapardos@gmail.com>
2021-05-27 16:25:17 -0300

    firebase context

---
+++ firebase/config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyDcpfXo8Mm1-joadTufYDWHp_tH3ehhEXE',
+  authDomain: 'ecommerce-misfans.firebaseapp.com',
+  projectId: 'ecommerce-misfans',
+  storageBucket: 'ecommerce-misfans.appspot.com',
+  messagingSenderId: '1017428698082',
+  appId: '1:1017428698082:web:0afd48b562ebaa575ecfbb',
+  measurementId: 'G-4QB0SM619G'
+}
+export default firebaseConfig
