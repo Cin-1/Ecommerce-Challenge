d38bbd5331183086f60b9ef48feb769d84a1ab85
Cin-1 <cinthiapardos@gmail.com>
2021-05-27 19:39:47 -0300

    login connected

--- pages/login.js
+++ pages/login.js
@@ -1,7 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 import * as Yup from 'yup'
 import Layout from '../components/layouts/layout'
 import { Formik, Form, Field, ErrorMessage } from 'formik'
+import firebase from '../firebase'
+import Router from 'next/router'
 
 const initialValues = {
   email: '',
@@ -17,58 +19,86 @@ const LoginSchema = Yup.object().shape({
 })
 
 const Login = () => {
+  const [userError, setUserError] = useState(false)
+
   return (
     <Formik
       initialValues={initialValues}
       validationSchema={LoginSchema}
-      onSubmit={values => {
-        console.log(values)
+      onSubmit={async function create(values) {
+        try {
+          await firebase.login(values.email, values.password)
+          Router.push('/')
+        } catch (error) {
+          console.error(error.message)
+          setUserError(error.message)
+        }
       }}
     >
       {formik => {
         const { errors, touched, isValid, dirty } = formik
         return (
-          <div className="container">
-            <h1>Login to continue</h1>
-            <Form>
-              <div className="form-row">
-                <label htmlFor="email">Email</label>
-                <Field
-                  type="email"
-                  name="email"
-                  id="email"
-                  className={
-                    errors.email && touched.email ? 'input-error' : null
-                  }
-                />
-                <ErrorMessage name="email" component="span" className="error" />
-              </div>
+          <div className="flex items-center justify-center bg-primary h-screen w-screen">
+            <div className="flex flex-col items-center justify-center bg-secondary h-96 w-80 rounded">
+              <h1 className="italic text-2xl text-neutral mb-6">
+                Login to continue
+              </h1>
+              <Form className="w-full px-4">
+                <div className="flex flex-col form-row mb-2 rounded">
+                  {userError && <p>{userError}</p>}
+                  <label htmlFor="email" className="text-neutral">
+                    Email
+                  </label>
+                  <Field
+                    type="email"
+                    name="email"
+                    id="email"
+                    className={
+                      errors.email && touched.email
+                        ? 'rounded-alert'
+                        : 'rounded'
+                    }
+                  />
+                  <div>
+                    <ErrorMessage
+                      name="email"
+                      component="span"
+                      className=" flex flex-col form-row  text-xs bg-danger text-danger px-2 w-full border-danger rounded py-1 my-1"
+                    />
+                  </div>
+                </div>
+                <div className="flex flex-col form-row rounded">
+                  <label htmlFor="password" className="text-neutral">
+                    Password
+                  </label>
+                  <Field
+                    type="password"
+                    name="password"
+                    id="password"
+                    className={
+                      errors.password && touched.password
+                        ? 'rounded-alert'
+                        : 'rounded'
+                    }
+                  />
+                  <div>
+                    <ErrorMessage
+                      name="password"
+                      component="span"
+                      className=" flex flex-col form-row text-xs bg-danger text-danger  px-2 w-full border-danger rounded py-1 my-1"
+                    />
+                  </div>
+                </div>
 
-              <div className="form-row">
-                <label htmlFor="password">Password</label>
-                <Field
-                  type="password"
-                  name="password"
-                  id="password"
-                  className={
-                    errors.password && touched.password ? 'input-error' : null
-                  }
-                />
-                <ErrorMessage
-                  name="password"
-                  component="span"
-                  className="error"
-                />
-              </div>
-
-              <button
-                type="submit"
-                className={!(dirty && isValid) ? 'disabled-btn' : ''}
-                disabled={!(dirty && isValid)}
-              >
-                Login
-              </button>
-            </Form>
+                <button
+                  type="submit"
+                  className="bg-success hover:bg-successhover duration-300 text-neutral w-full mt-10 rounded"
+                  disabled={!(dirty && isValid)}
+                >
+                  Login
+                </button>
+              </Form>
+            </div>
           </div>
         )
       }}
