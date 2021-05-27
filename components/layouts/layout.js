5018c0cc7ba3ce248b89aeccd787c821065f2dc7
Cin-1 <cinthiapardos@gmail.com>
2021-05-27 15:19:36 -0300

    layout

---
+++ components/layouts/layout.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import Link from 'next/link'
+import Head from 'next/head'
+import NavBar from '../NavBar'
+
+const Layout = props => {
+  return (
+    <>
+      <Head>
+        <title>Mis Fans</title>
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Montserrat:wght@200&display=swap"
+          rel="stylesheet"
+        />
+        <link href="/static/css/app.css" rel="stylesheet" />
+      </Head>
+
+      <main>{props.children}</main>
+      <NavBar />
+    </>
+  )
+}
+
+export default Layout
