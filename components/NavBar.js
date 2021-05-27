fc1c057197890a760346ef83a6888cf753fd564b
Cin-1 <cinthiapardos@gmail.com>
2021-05-27 15:18:57 -0300

    navbar added

---
+++ components/NavBar.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import Link from 'next/link'
+
+const NavBar = () => {
+  return (
+    <nav className="flex p-1 justify-between">
+      <Link href="/">
+        <a className="flex p-1">Inicio</a>
+      </Link>
+      <Link href="/populares">
+        <a className="flex p-1">Populares</a>
+      </Link>
+      <Link href="/nuevo-producto">
+        <a className="flex p-1">Nuevo producto</a>
+      </Link>
+      <p className="flex p-1">Hello Cin! </p>
+      <button className="flex p-1">Logout</button>
+      <Link href="/login" className="flex p-1">
+        <button bgColor="true">Login</button>
+      </Link>
+      <Link href="/register" className="flex p-1">
+        <button className="flex p-1 flex justify-end ">Create Account</button>
+      </Link>
+    </nav>
+  )
+}
+
+export default NavBar
