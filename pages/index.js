049cd35c2f2f42d4ccc05aac170f69a10189f01b
Cin-1 <cinthiapardos@gmail.com>
2021-05-28 15:49:07 -0300

    firebase storage

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Layout from '../components/layouts/layout'
 import Carrousel from '../components/carousel'
+import Example from '../components/gridAdmin'
 
 export default function Home() {
   return (
@@ -7,6 +8,7 @@ export default function Home() {
       <Layout />
 
       <Carrousel />
+      <Example />
     </div>
   )
 }
