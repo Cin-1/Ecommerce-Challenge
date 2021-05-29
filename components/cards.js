2a1dd2f34f6547d36b00ac20b11f20f85129f501
Cin-1 <cinthiapardos@gmail.com>
2021-05-29 00:26:36 -0300

    card seller

---
+++ components/cards.js
@@ -0,0 +1,178 @@
+import React from 'react'
+import Link from 'next/link'
+import Layout from './layouts/layout'
+
+function Index() {
+  return (
+    <>
+      <div className="flex flex-wrap bg-primary">
+        <div className="flex flex-col px-3 py-16 m-5 rounded-lg xl:w-1/4 sm:w-1/2 2xl:w-1/5 md:py-12 bg-gradient-to-r from-indigo-700 to-purple-500">
+          <div className="flex items-center justify-center ">
+            <div className="flex flex-col items-center">
+              <img
+                src="https://cdn.tuk.dev/assets/templates/olympus/profile.png"
+                alt="profile"
+              />
+              <p className="mt-2 text-xs font-semibold text-center text-white sm:text-sm md:text-base">
+                Ricardo Boveta
+              </p>
+            </div>
+          </div>
+          <div className="flex items-center mt-7">
+            <div className>
+              <p className="text-xs text-gray-300">Products</p>
+              <p className="mt-2 text-base sm:text-lg md:text-xl 2xl:text-2xl text-gray-50">
+                28
+              </p>
+            </div>
+            <div className="ml-12">
+              <p className="text-xs text-gray-300">Revenue</p>
+              <p className="mt-2 text-base sm:text-lg md:text-xl 2xl:text-2xl text-gray-50">
+                $2890
+              </p>
+            </div>
+            <div className="ml-12">
+              <p className="text-xs text-gray-300">Average</p>
+              <p className="mt-2 text-base sm:text-lg md:text-xl 2xl:text-2xl text-gray-50">
+                $169
+              </p>
+            </div>
+          </div>
+        </div>{' '}
+        <div className="flex flex-col px-3 py-16 m-5 rounded-lg xl:w-1/4 sm:w-1/2 2xl:w-1/5 md:py-12 bg-gradient-to-r from-indigo-700 to-purple-500">
+          <div className="flex items-center justify-center ">
+            <div className="flex flex-col items-center">
+              <img
+                src="https://cdn.tuk.dev/assets/templates/olympus/profile.png"
+                alt="profile"
+              />
+              <p className="mt-2 text-xs font-semibold text-center text-white sm:text-sm md:text-base">
+                Ricardo Boveta
+              </p>
+            </div>
+          </div>
+          <div className="flex items-center mt-7">
+            <div className>
+              <p className="text-xs text-gray-300">Products</p>
+              <p className="mt-2 text-base sm:text-lg md:text-xl 2xl:text-2xl text-gray-50">
+                28
+              </p>
+            </div>
+            <div className="ml-12">
+              <p className="text-xs text-gray-300">Revenue</p>
+              <p className="mt-2 text-base sm:text-lg md:text-xl 2xl:text-2xl text-gray-50">
+                $2890
+              </p>
+            </div>
+            <div className="ml-12">
+              <p className="text-xs text-gray-300">Average</p>
+              <p className="mt-2 text-base sm:text-lg md:text-xl 2xl:text-2xl text-gray-50">
+                $169
+              </p>
+            </div>
+          </div>
+        </div>{' '}
+        <div className="flex flex-col px-3 py-16 m-5 rounded-lg xl:w-1/4 sm:w-1/2 2xl:w-1/5 md:py-12 bg-gradient-to-r from-indigo-700 to-purple-500">
+          <div className="flex items-center justify-center ">
+            <div className="flex flex-col items-center">
+              <img
+                src="https://cdn.tuk.dev/assets/templates/olympus/profile.png"
+                alt="profile"
+              />
+              <p className="mt-2 text-xs font-semibold text-center text-white sm:text-sm md:text-base">
+                Ricardo Boveta
+              </p>
+            </div>
+          </div>
+          <div className="flex items-center mt-7">
+            <div className>
+              <p className="text-xs text-gray-300">Products</p>
+              <p className="mt-2 text-base sm:text-lg md:text-xl 2xl:text-2xl text-gray-50">
+                28
+              </p>
+            </div>
+            <div className="ml-12">
+              <p className="text-xs text-gray-300">Revenue</p>
+              <p className="mt-2 text-base sm:text-lg md:text-xl 2xl:text-2xl text-gray-50">
+                $2890
+              </p>
+            </div>
+            <div className="ml-12">
+              <p className="text-xs text-gray-300">Average</p>
+              <p className="mt-2 text-base sm:text-lg md:text-xl 2xl:text-2xl text-gray-50">
+                $169
+              </p>
+            </div>
+          </div>
+        </div>{' '}
+        <div className="flex flex-col px-3 py-16 m-5 rounded-lg xl:w-1/4 sm:w-1/2 2xl:w-1/5 md:py-12 bg-gradient-to-r from-indigo-700 to-purple-500">
+          <div className="flex items-center justify-center ">
+            <div className="flex flex-col items-center">
+              <img
+                src="https://cdn.tuk.dev/assets/templates/olympus/profile.png"
+                alt="profile"
+              />
+              <p className="mt-2 text-xs font-semibold text-center text-white sm:text-sm md:text-base">
+                Ricardo Boveta
+              </p>
+            </div>
+          </div>
+          <div className="flex items-center mt-7">
+            <div className>
+              <p className="text-xs text-gray-300">Products</p>
+              <p className="mt-2 text-base sm:text-lg md:text-xl 2xl:text-2xl text-gray-50">
+                28
+              </p>
+            </div>
+            <div className="ml-12">
+              <p className="text-xs text-gray-300">Revenue</p>
+              <p className="mt-2 text-base sm:text-lg md:text-xl 2xl:text-2xl text-gray-50">
+                $2890
+              </p>
+            </div>
+            <div className="ml-12">
+              <p className="text-xs text-gray-300">Average</p>
+              <p className="mt-2 text-base sm:text-lg md:text-xl 2xl:text-2xl text-gray-50">
+                $169
+              </p>
+            </div>
+          </div>
+        </div>{' '}
+        <div className="flex flex-col px-3 py-16 m-5 rounded-lg xl:w-1/4 sm:w-1/2 2xl:w-1/5 md:py-12 bg-gradient-to-r from-indigo-700 to-purple-500">
+          <div className="flex items-center justify-center ">
+            <div className="flex flex-col items-center">
+              <img
+                src="https://cdn.tuk.dev/assets/templates/olympus/profile.png"
+                alt="profile"
+              />
+              <p className="mt-2 text-xs font-semibold text-center text-white sm:text-sm md:text-base">
+                Ricardo Boveta
+              </p>
+            </div>
+          </div>
+          <div className="flex items-center mt-7">
+            <div className>
+              <p className="text-xs text-gray-300">Products</p>
+              <p className="mt-2 text-base sm:text-lg md:text-xl 2xl:text-2xl text-gray-50">
+                28
+              </p>
+            </div>
+            <div className="ml-12">
+              <p className="text-xs text-gray-300">Revenue</p>
+              <p className="mt-2 text-base sm:text-lg md:text-xl 2xl:text-2xl text-gray-50">
+                $2890
+              </p>
+            </div>
+            <div className="ml-12">
+              <p className="text-xs text-gray-300">Average</p>
+              <p className="mt-2 text-base sm:text-lg md:text-xl 2xl:text-2xl text-gray-50">
+                $169
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+export default Index
