3371e71b0e99b1682b1d214e777c8fe5cdc82827
Cin-1 <cinthiapardos@gmail.com>
2021-05-30 16:26:21 -0300

    cart with provider

--- components/cart.js
+++ components/cart.js
@@ -10,11 +10,20 @@ import {
   AiOutlineEdit,
   AiOutlineUserAdd
 } from 'react-icons/ai'
+import { CartProvider, useCart } from 'react-use-cart'
 import Link from 'next/link'
 
 export default function Table() {
   const { user, firebase } = useContext(FirebaseContext)
   const [people, setPeople] = useState([])
+  const {
+    isEmpty,
+    totalUniqueItems,
+    items,
+    updateItemQuantity,
+    removeItem,
+    emptyCart
+  } = useCart()
 
   const getSellers = () => {
     firebase.db.collection('sellers').orderBy('name').onSnapshot(handleSnap)
@@ -33,7 +42,7 @@ export default function Table() {
     })
     setPeople(people)
   }
-  const deleteSeller = id => {
+  const remove = () => {
     Swal.fire({
       title: 'Are you sure?',
       icon: 'warning',
@@ -43,18 +52,16 @@ export default function Table() {
       confirmButtonText: 'Yes, remove it!'
     }).then(result => {
       if (result.isConfirmed) {
-        try {
-          firebase.db.collection('sellers').doc(id).delete()
-        } catch (err) {
-          console.log(err)
-        }
+        emptyCart()
       }
     })
   }
-
+  if (isEmpty) return <p>Your cart is empty</p>
   return (
     <div className="flex flex-col mx-5">
       <div className="overflow-x-auto">
+        <h1>Cart ({totalUniqueItems})</h1>
+
         <div className="inline-block w-full align-middle">
           <div className="mx-1 overflow-hidden sm:rounded-lg">
             <table className="w-full divide-y divide-primary">
@@ -65,7 +72,23 @@ export default function Table() {
                     className="py-3 text-xs font-medium tracking-wider text-left uppercase text-neutral"
                   >
                     Name
-                  </th>{' '}
+                  </th>
+                  <th
+                    scope="col"
+                    className="py-3 text-xs font-medium tracking-wider text-left uppercase text-neutral"
+                  >
+                    Price
+                  </th>
+                  <th
+                    scope="col"
+                    className="py-3 text-xs font-medium tracking-wider text-left uppercase text-neutral"
+                  ></th>
+                  <th
+                    scope="col"
+                    className="py-3 text-xs font-medium tracking-wider text-left uppercase text-neutral"
+                  >
+                    quantity
+                  </th>
                   <th
                     scope="col"
                     className="py-3 text-xs font-medium tracking-wider text-left uppercase text-neutral"
@@ -74,15 +97,24 @@ export default function Table() {
                     scope="col"
                     className="py-3 text-xs font-medium tracking-wider text-left uppercase text-neutral"
                   ></th>
-                  <th scope="col" className="relative py-3"></th>
                   <th
                     scope="col"
                     className="py-3 text-xs font-medium tracking-wider text-left uppercase text-neutral"
-                  ></th>
+                  >
+                    Total
+                  </th>
                   <th
                     scope="col"
                     className="py-3 text-lg font-medium tracking-wider text-left uppercase ml-5text-xs text-neutral"
                   >
+                    <button
+                      onClick={() => {
+                        remove()
+                      }}
+                      className="inline-block px-4 py-3 mt-4 mr-3 text-sm leading-none duration-300 rounded text-neutral hover:text-neutral bg-success hover:bg-successhover lg:mt-0"
+                    >
+                      EMPTY CART{' '}
+                    </button>
                     <button className="inline-block px-4 py-3 mt-4 mr-3 text-sm leading-none duration-300 rounded text-neutral hover:text-neutral bg-success hover:bg-successhover lg:mt-0">
                       CHECKOUT
                     </button>
@@ -90,34 +122,48 @@ export default function Table() {
                 </tr>
               </thead>
               <tbody className="divide-y divide-primary bg-secondary">
-                {people.map(person => (
-                  <tr key={person.id}>
+                {items.map(item => (
+                  <tr key={item.id}>
                     <td className="py-4 whitespace-nowrap">
                       <div className="flex items-center">
                         <div className="flex-shrink-0 w-10 h-10">
                           <img
                             className="w-10 h-10 rounded-full"
-                            src={person.urlImg}
-                            alt=""
+                            src={item.urlImg}
+                            alt="product"
                           />
                         </div>
                         <div className="ml-4">
                           <div className="text-sm font-medium text-neutral">
-                            {person.name}
+                            {item.name}
                           </div>
                         </div>
                       </div>
                     </td>
-
                     <td className="py-4 whitespace-nowrap">
                       <button className="text-lg text-neutral">
+                        <p>{item.price}</p>
+                      </button>
+                    </td>
+                    <td className="py-4 whitespace-nowrap">
+                      <button
+                        className="text-lg text-neutral"
+                        onClick={() =>
+                          updateItemQuantity(item.id, item.quantity + 1)
+                        }
+                      >
                         <AiOutlinePlusSquare />
                       </button>
                     </td>
-                    <td className="py-4 text-lg text-neutral whitespace-nowrap">
-                      <p>1</p>
+                    <td className="py-4 text-lg text-neutral whitespace-wrap">
+                      <p>{item.quantity}</p>
                     </td>
-                    <td className="py-4 whitespace-nowrap">
+                    <td
+                      className="py-4 whitespace-nowrap"
+                      onClick={() =>
+                        updateItemQuantity(item.id, item.quantity - 1)
+                      }
+                    >
                       <button className="text-lg text-neutral">
                         <AiOutlineMinusSquare />
                       </button>
@@ -127,11 +173,14 @@ export default function Table() {
                       <button
                         className="text-lg text-neutral"
                         onClick={() => {
-                          deleteSeller(person.id)
+                          removeItem(item.id)
                         }}
                       >
                         <AiOutlineDelete />
                       </button>
+                    </td>
+                    <td className="py-4 text-lg text-neutral whitespace-nowrap">
+                      <p>{parseInt(item.price) * parseInt(item.quantity)}</p>
                     </td>
                   </tr>
                 ))}
--- pages/_app.js
+++ pages/_app.js
@@ -6,12 +6,15 @@ import 'swiper/swiper-bundle.css'
 import 'swiper/components/autoplay/package.json'
 import 'swiper/components/navigation/package.json'
 import 'swiper/components/pagination/package.json'
+import { CartProvider } from 'react-use-cart'
 
 function MyApp({ Component, pageProps }) {
   const user = useAuth()
   return (
     <FirebaseContext.Provider value={{ firebase, user }}>
-      <Component {...pageProps} />
+      <CartProvider>
+        <Component {...pageProps} />
+      </CartProvider>
     </FirebaseContext.Provider>
   )
 }
