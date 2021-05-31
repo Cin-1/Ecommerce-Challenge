d82adda4ac7d283c40a51bcbe7c770730b7d1e89
Cin-1 <cinthiapardos@gmail.com>
2021-05-30 22:40:13 -0300

    cart conected

--- components/productsCard.js
+++ components/productsCard.js
@@ -1,51 +1,61 @@
 import React, { useContext } from 'react'
 import { FirebaseContext } from '../firebase'
+import { useCart } from 'react-use-cart'
 import Link from 'next/link'
 
 function ProductsCard({ product }) {
-  const { user, firebase } = useContext(FirebaseContext)
+  const { user } = useContext(FirebaseContext)
+  const { addItem, inCart, items } = useCart()
+
   return (
     <>
-      <div className="flex flex-col px-3 py-16 m-5 rounded-lg xl:w-1/4 2xl:w-1/5 md:py-12 bg-gradient-to-r from-indigo-700 to-purple-500">
-        <div className="flex items-center justify-center ">
+      <div className="flex flex-col w-64 m-5 rounded-lg h-96 xl:w-1/4 2xl:w-1/5 md:py-12 bg-secondary text-neutral">
+        <div className="flex items-center justify-center pt-4">
           <div className="flex flex-col items-center">
             <img
               src={product.urlImg}
               alt="profile"
-              className="w-16 h-16 rounded-full"
+              className="object-cover w-full h-32 px-4 mb-4 rounded-2xl"
             />
-            <p className="mt-2 text-xs font-semibold text-center text-white sm:text-sm md:text-base">
+            <p className="mt-2 text-lg font-semibold text-center capitalize text-success sm:text-sm md:text-base font-montserrat">
               {product.product}
             </p>
-            <p className="mt-2 text-xs text-center text-white font sm:text-sm md:text-base">
+            <p className="w-3/4 h-16 mt-2 text-xs text-center text-neutral font sm:text-sm md:text-base font-montserrat">
               {product.description}
+            </p>
+
+            <p className="text-base font-bold text-light font-montserrat">
+              Price
+            </p>
+            <p className="h-10 text-lg font-semibold font-bold text-center text-white sm:text-sm md:text-base">
+              $ {product.price}
             </p>
           </div>
         </div>
-        <div className="flex items-center mb-5 mt-7">
-          <div className="ml-6"></div>
-          <div className="ml-6"></div>
-          <div className="ml-6"></div>
-        </div>
         {user ? (
-          <>
-            <Link href="/products[id]" as={`/products/${product.id}`}>
-              <button className="inline-block px-4 py-2 mt-5 mr-3 text-sm leading-none duration-300 rounded text-neutral hover:text-neutral bg-success hover:bg-successhover lg:mt-0">
-                Add to cart
-              </button>
-            </Link>
-            <Link href="/products[id]" as={`/products/${product.id}`}>
-              <button className="inline-block px-4 py-2 mt-5 mr-3 text-sm leading-none duration-300 rounded text-neutral hover:text-neutral bg-success hover:bg-successhover lg:mt-0">
+          <div className="flex px-4 font-montserrat">
+            <button
+              className="inline-block w-1/2 px-4 py-2 mr-2 text-sm font-bold leading-none duration-300 rounded text-neutral hover:text-neutral bg-success hover:bg-successhover lg:mt-0"
+              onClick={() => addItem(product)}
+              disabled={inCart(product.id)}
+            >
+              {inCart(product.id) ? 'Already in cart' : 'Add to cart'}
+            </button>
+            <Link href="/checkout">
+              <button className="inline-block w-3/5 px-4 py-2 text-sm font-bold leading-none duration-300 rounded text-neutral hover:text-neutral bg-success hover:bg-successhover lg:mt-0">
                 Proceed to checkout
               </button>
             </Link>
-          </>
+          </div>
         ) : (
-          <button className="inline-block px-4 py-3 mt-10 mr-3 text-sm leading-none duration-300 rounded text-neutral hover:text-neutral bg-success hover:bg-successhover lg:mt-0">
-            Login to buy!
-          </button>
+          <div className="flex px-4">
+            <button className="self-center inline-block w-full py-3 text-sm font-bold leading-none duration-300 rounded text-neutral hover:text-neutral bg-success hover:bg-successhover lg:mt-0">
+              Login to buy!
+            </button>
+          </div>
         )}
       </div>
+      )
     </>
   )
 }
---
+++ pages/products/new.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Layout from '../../components/layouts/layout'
+import NewSeller from '../../components/newSeller'
+
+const Admin = () => {
+  return (
+    <div className="bg-primary">
+      <Layout />
+      <NewSeller />
+    </div>
+  )
+}
+
+export default Admin
